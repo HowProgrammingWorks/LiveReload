062bc8e923a73d765dc87a60a1eea1b993828674
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-26 03:26:10 +0200

    Example with autoreload and introspection

---
+++ JavaScript/lib/submodule3.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const method31 = () => 31;
+const method32 = () => 32;
+
+module.exports = { method31, method32 };
---
+++ JavaScript/server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+
+const cache = new Map();
+const lib = './lib/';
+
+const cacheFile = (path) => {
+  const filePath = lib + path;
+  try {
+    const libPath = require.resolve(filePath);
+    delete require.cache[libPath];
+  } catch (e) {
+    return;
+  }
+  try {
+    const mod = require(filePath);
+    cache.set(path, mod);
+  } catch (e) {
+    cache.delete(path);
+  }
+};
+
+const cacheFolder = (path) => {
+  fs.readdir(path, (err, files) => {
+    if (err) return;
+    files.forEach(cacheFile);
+  });
+};
+
+const watch = (path) => {
+  fs.watch(path, (event, file) => {
+    cacheFile(file);
+  });
+};
+
+cacheFolder(lib);
+watch(lib);
+
+const ls = (res, list) => {
+  res.write('<html>');
+  for (const name of list) {
+    res.write(`<li><a href="${name}/">${name}</li>`);
+  }
+  res.end('</html>');
+};
+
+http.createServer((req, res) => {
+  const url = req.url.substring(1);
+  if (!url) return ls(res, cache.keys());
+  const [mod, method] = url.split('/');
+  const methods = cache.get(mod);
+  if (methods) {
+    if (!method) return ls(res, Object.keys(methods));
+    const handler = methods[method];
+    if (handler) {
+      res.end(handler().toString());
+      return;
+    }
+  }
+  res.end('File ' + url + 'not found');
+}).listen(8000);
