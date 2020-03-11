5a3723ea2a2f57b88133a44a5deda7b6c454bb8f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-03-11 12:53:48 +0200

    Fix eslint issue (workaround)

--- JavaScript/lib/submodule1.js
+++ JavaScript/lib/submodule1.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const method11 = () => 11;
+const method11 = () => {
+  const result = Object.keys(global);
+  return result;
+};
 const method12 = () => 12;
 
 module.exports = { method11, method12 };
